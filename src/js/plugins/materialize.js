6678fbffd44a7c9597868e919b489cb890ac0e56
Liubov <kortcova@gmail.com>
2022-08-10 13:03:52 +0300

    add style/materialize

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -7,3 +7,17 @@ M.FormSelect.init(select);
 export function getSelectInstance(elem) {
   return M.FormSelect.getInstance(elem);
 }
+
+const autocomplete = document.querySelector('.autocomplete');
+M.Autocomplete.init(autocomplete);
+
+export function getAutocompleteInstance(elem) {
+  return M.Autocomplete.getInstance(elem);
+}
+
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker);
+
+export function getDatepickerInstance(elem) {
+  return M.Datepicker.getInstance(elem);
+}
